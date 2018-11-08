b7259e0361c28106a8e65eaebe20eedc87b99ee5
fcampara <fcamparasilva@gmail.com>
2018-11-08 17:24:59 -0400

    Fix(GitIngore) Insert in gitignore to ignore database config

--- delivery-api/src/domain/repositories/user.js
+++ delivery-api/src/domain/repositories/user.js
@@ -5,14 +5,16 @@ const getError = require('../../http/middlewares/error/MongoDB')
 module.exports = () => {
   class UserRepository {
     static async save (data) {
-      const user = new User(data)
-      user.save(err => {
-        if(err) {
-          logger.error(JSON.stringify(err))
-          const msg = getError[err.code]
-        } else {
-          console.log('Sucesso')
-        }
+      return new Promise((resolve, reject) => {
+        const user = new User(data)
+        user.save(err => {
+          if(err) {
+            logger.error(JSON.stringify(err))
+            getError[err.code]
+          } else {
+            
+          }
+        })
       })
     }
 
