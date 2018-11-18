57f53ab299c29fa571d85e1a5658431ae859f14a
felipe <fcamparasilva@gmail.com>
2018-11-18 20:12:36 -0300

    Fix(Folder and names): Fix folder logs and fix error in database

--- src/http/middlewares/logger.js
+++ src/http/middlewares/logger.js
@@ -5,19 +5,18 @@ var fs = require('fs');
 // to whatever file references it.
 var Logger = exports.Logger = {};
 
-
 // Create 3 sets of write streams for the 3 levels of logging we wish to do
 // every time we get an error we'll append to our error streams, any debug message
 // to our debug stream etc...
-var infoStream = fs.createWriteStream(`${__dirname}/../../logs/info.txt`);
+var infoStream = fs.createWriteStream(`${__dirname}/../../../logs/info.txt`);
 // Notice we set the path of our log files in the first parameter of
 // fs.createWriteStream. This could easily be pulled in from a config
 // file if needed.
-var errorStream = fs.createWriteStream(`${__dirname}/../../logs/error.txt`);
+var errorStream = fs.createWriteStream(`${__dirname}/../../../logs/error.txt`);
 // createWriteStream takes in options as a second, optional parameter
 // if you wanted to set the file encoding of your output file you could
 // do so by setting it like so: ('logs/debug.txt' , { encoding : 'utf-8' });
-var debugStream = fs.createWriteStream(`${__dirname}/../../logs/debug.txt`);
+var debugStream = fs.createWriteStream(`${__dirname}/../../../logs/debug.txt`);
 
 
 // Finally we create 3 different functions
